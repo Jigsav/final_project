9a6070b0c84de7bedab84984039b241fe3ffbdaf
Jigsav <pashin.v.s@gmail.com>
2022-01-08 22:49:56 +0300

    меньше 20 попыток

--- game_v2.py
+++ game_v2.py
@@ -15,12 +15,18 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
+    min_number = 1
+    max_number = 101
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        predict_number = np.random.randint(min_number, max_number)  # предполагаемое число
         if number == predict_number:
             break  # выход из цикла если угадали
+        elif predict_number < number:
+            min_number = predict_number
+        elif predict_number > number:
+            max_number = predict_number
     return count
 
 
